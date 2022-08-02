091c6e5b9a788a2c0653e658608334d57ea12072
dhambure98 <akila789liyanage@gmail.com>
2022-08-02 09:42:28 +0530

    trying getting server path localhost

---
+++ app.js
@@ -0,0 +1,21 @@
+// https://expressjs.com/en/starter/hello-world.html
+
+const express = require('express')
+const app = express()
+const port = 4000
+
+//localhost req 
+app.get('/', (req, res) => {
+    console.log('get request comming!');
+    res.send('get req came for / route')
+})
+
+//localhost req for customer
+app.get('/customer', (req, res) => {
+    console.log('customer get come');
+    res.send('<h1>Customer get req came</h1>')
+})
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`);
+})
